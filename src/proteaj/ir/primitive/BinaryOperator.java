f06e06db7743b293109fe4fd0aac3a89ef162c52
Kazuhiro Ichikawa <ichikawa@csg.ci.i.u-tokyo.ac.jp>
2014-05-15 21:50:12 +0900

    int以外のプリミティブ型の不等号を追加

--- src/proteaj/ir/primitive/BinaryOperator.java
+++ src/proteaj/ir/primitive/BinaryOperator.java
@@ -13,10 +13,38 @@ public class BinaryOperator extends PrimitiveOperator {
   public static final BinaryOperator mul    = new BinaryOperator(CtClass.intType, "*", 1000);
   public static final BinaryOperator div    = new BinaryOperator(CtClass.intType, "/", 1000);
   public static final BinaryOperator mod    = new BinaryOperator(CtClass.intType, "%", 1000);
-  public static final BinaryOperator lthan  = new BinaryOperator(CtClass.booleanType, CtClass.intType, "<", CtClass.intType, 700);
-  public static final BinaryOperator gthan  = new BinaryOperator(CtClass.booleanType, CtClass.intType, ">", CtClass.intType, 700);
-  public static final BinaryOperator leq    = new BinaryOperator(CtClass.booleanType, CtClass.intType, "<=", CtClass.intType, 700);
-  public static final BinaryOperator geq    = new BinaryOperator(CtClass.booleanType, CtClass.intType, ">=", CtClass.intType, 700);
+
+  public static final BinaryOperator blt  = new BinaryOperator(CtClass.booleanType, CtClass.byteType, "<", CtClass.byteType, 700);
+  public static final BinaryOperator clt  = new BinaryOperator(CtClass.booleanType, CtClass.charType, "<", CtClass.charType, 700);
+  public static final BinaryOperator slt  = new BinaryOperator(CtClass.booleanType, CtClass.shortType, "<", CtClass.shortType, 700);
+  public static final BinaryOperator ilt  = new BinaryOperator(CtClass.booleanType, CtClass.intType, "<", CtClass.intType, 700);
+  public static final BinaryOperator llt  = new BinaryOperator(CtClass.booleanType, CtClass.longType, "<", CtClass.longType, 700);
+  public static final BinaryOperator flt  = new BinaryOperator(CtClass.booleanType, CtClass.floatType, "<", CtClass.floatType, 700);
+  public static final BinaryOperator dlt  = new BinaryOperator(CtClass.booleanType, CtClass.doubleType, "<", CtClass.doubleType, 700);
+
+  public static final BinaryOperator bgt  = new BinaryOperator(CtClass.booleanType, CtClass.byteType, ">", CtClass.byteType, 700);
+  public static final BinaryOperator cgt  = new BinaryOperator(CtClass.booleanType, CtClass.charType, ">", CtClass.charType, 700);
+  public static final BinaryOperator sgt  = new BinaryOperator(CtClass.booleanType, CtClass.shortType, ">", CtClass.shortType, 700);
+  public static final BinaryOperator igt  = new BinaryOperator(CtClass.booleanType, CtClass.intType, ">", CtClass.intType, 700);
+  public static final BinaryOperator lgt  = new BinaryOperator(CtClass.booleanType, CtClass.longType, ">", CtClass.longType, 700);
+  public static final BinaryOperator fgt  = new BinaryOperator(CtClass.booleanType, CtClass.floatType, ">", CtClass.floatType, 700);
+  public static final BinaryOperator dgt  = new BinaryOperator(CtClass.booleanType, CtClass.doubleType, ">", CtClass.doubleType, 700);
+
+  public static final BinaryOperator bleq  = new BinaryOperator(CtClass.booleanType, CtClass.byteType, "<=", CtClass.byteType, 700);
+  public static final BinaryOperator cleq  = new BinaryOperator(CtClass.booleanType, CtClass.charType, "<=", CtClass.charType, 700);
+  public static final BinaryOperator sleq  = new BinaryOperator(CtClass.booleanType, CtClass.shortType, "<=", CtClass.shortType, 700);
+  public static final BinaryOperator ileq  = new BinaryOperator(CtClass.booleanType, CtClass.intType, "<=", CtClass.intType, 700);
+  public static final BinaryOperator lleq  = new BinaryOperator(CtClass.booleanType, CtClass.longType, "<=", CtClass.longType, 700);
+  public static final BinaryOperator fleq  = new BinaryOperator(CtClass.booleanType, CtClass.floatType, "<=", CtClass.floatType, 700);
+  public static final BinaryOperator dleq  = new BinaryOperator(CtClass.booleanType, CtClass.doubleType, "<=", CtClass.doubleType, 700);
+
+  public static final BinaryOperator bgeq  = new BinaryOperator(CtClass.booleanType, CtClass.byteType, ">=", CtClass.byteType, 700);
+  public static final BinaryOperator cgeq  = new BinaryOperator(CtClass.booleanType, CtClass.charType, ">=", CtClass.charType, 700);
+  public static final BinaryOperator sgeq  = new BinaryOperator(CtClass.booleanType, CtClass.shortType, ">=", CtClass.shortType, 700);
+  public static final BinaryOperator igeq  = new BinaryOperator(CtClass.booleanType, CtClass.intType, ">=", CtClass.intType, 700);
+  public static final BinaryOperator lgeq  = new BinaryOperator(CtClass.booleanType, CtClass.longType, ">=", CtClass.longType, 700);
+  public static final BinaryOperator fgeq  = new BinaryOperator(CtClass.booleanType, CtClass.floatType, ">=", CtClass.floatType, 700);
+  public static final BinaryOperator dgeq  = new BinaryOperator(CtClass.booleanType, CtClass.doubleType, ">=", CtClass.doubleType, 700);
 
   public static final BinaryOperator bequal = new BinaryOperator(CtClass.booleanType, CtClass.byteType, "==", CtClass.byteType, 600);
   public static final BinaryOperator cequal = new BinaryOperator(CtClass.booleanType, CtClass.charType, "==", CtClass.charType, 600);
