159e61b1140ac9b6ae498ebb27c278ad4f2f5436
Kazuhiro Ichikawa <ichikawa@csg.ci.i.u-tokyo.ac.jp>
2014-05-25 13:44:21 +0900

    JavaExpressionParser 整理、わずかにパフォーマンスを改善 (?)

--- src/proteaj/pparser/JavaExpressionParsers.java
+++ src/proteaj/pparser/JavaExpressionParsers.java
@@ -4,6 +4,7 @@ import javassist.*;
 
 import proteaj.error.*;
 import proteaj.tast.*;
+import proteaj.util.Pair;
 
 import static proteaj.pparser.PackratParserCombinators.*;
 import static proteaj.pparser.CommonParsers.*;
@@ -33,8 +34,10 @@ public class JavaExpressionParsers {
         else return failure("not array type");
       });
 
+  private static final PackratParser<Pair<Expression, String>> exprDotIdentifier = infix(ref_DotAccess, ".", identifier);
+
   private static final PackratParser<MethodCall> methodCall =
-      bind(infix(ref_DotAccess, ".", identifier), pair -> depends(env ->
+      bind(exprDotIdentifier, pair -> depends(env ->
           foreach(env.getInstanceMethods(pair._1.type, pair._2), method -> methodCallArgs(pair._1, method), "method " + pair._2 + " is not found in " + pair._1.type.getName())));
 
   private static PackratParser<MethodCall> methodCallArgs (Expression receiver, CtMethod method) {
@@ -45,21 +48,7 @@ public class JavaExpressionParsers {
   }
 
   private static final PackratParser<FieldAccess> fieldAccess =
-      bind(infix(ref_DotAccess, ".", identifier), pair -> depends(env -> {
-        CtField field;
-        try { field = pair._1.type.getField(pair._2); }
-        catch (NotFoundException e) {
-          return failure("field " + pair._2 + " is not found in " + pair._1.type.getName());
-        }
-
-        if (env.isVisible(field)) {
-          if (! isStatic(field)) try {
-            return unit(new FieldAccess(pair._1, field));
-          } catch (NotFoundException e) { return error(e); }
-          else return failure("field " + pair._1.type.getName() + '.' + pair._2 + " is a static field");
-        }
-        else return failure("field " + pair._1.type.getName() + '.' + pair._2 + " is not visible from " + env.thisClass.getName());
-      }));
+      bind(exprDotIdentifier, pair -> depends(env -> getInstanceField(pair._1, pair._2, env)));
 
   private static final PackratParser<Expression> arrayIndex = enclosed("[", expression(CtClass.intType), "]");
 
@@ -90,25 +79,39 @@ public class JavaExpressionParsers {
   private static final PackratParser<Expression> variable =
       bind(identifier, s -> depends(env -> env.contains(s) ? unit(env.get(s)) : failure("unknown variable: " + s)));
 
+  private static final PackratParser<Pair<CtClass, String>> classDotIdentifier = infix(className, ".", identifier);
+
   private static final PackratParser<StaticMethodCall> staticMethodCall =
-      bind(infix(className, ".", identifier), pair -> depends(env -> foreach(env.getStaticMethods(pair._1, pair._2), method -> staticMethodCallArgs(method), "suitable static method is not found")));
+      bind(classDotIdentifier, pair -> depends(env -> foreach(env.getStaticMethods(pair._1, pair._2), method -> staticMethodCallArgs(method), "suitable static method is not found")));
 
   private static final PackratParser<StaticFieldAccess> staticFieldAccess =
-      bind(infix(className, ".", identifier), pair -> depends(env -> {
-        CtField field;
-        try { field = pair._1.getField(pair._2); }
-        catch (NotFoundException e) {
-          return failure("field " + pair._2 + " is not found in " + pair._1.getName());
-        }
+      bind(classDotIdentifier, pair -> depends(env -> getStaticField(pair._1, pair._2, env)));
 
-        if (env.isVisible(field)) {
-          if (isStatic(field)) try {
-            return unit(new StaticFieldAccess(field));
-          } catch (NotFoundException e) { return error(e); }
-          else return failure("field " + pair._1.getName() + '.' + pair._2 + " is not a static field");
-        }
-        else return failure("field " + pair._1.getName() + '.' + pair._2 + " is not visible from " + env.thisClass.getName());
-      }));
+  private static PackratParser<FieldAccess> getInstanceField (Expression expr, String name, Environment env) {
+    CtField field;
+    try { field = expr.type.getField(name); } catch (NotFoundException e) {
+      return failure("field " + name + " is not found in " + expr.type.getName());
+    }
+    if (! env.isVisible(field)) return failure("field " + expr.type.getName() + '.' + name + " is not visible from " + env.thisClass.getName());
+    if (isStatic(field)) return failure("field " + expr.type.getName() + '.' + name + " is a static field");
+
+    try { return unit(new FieldAccess(expr, field)); } catch (NotFoundException e) {
+      return error(e);
+    }
+  }
+
+  private static PackratParser<StaticFieldAccess> getStaticField (CtClass clazz, String name, Environment env) {
+    CtField field;
+    try { field = clazz.getField(name); } catch (NotFoundException e) {
+      return failure("field " + name + " is not found in " + clazz.getName());
+    }
+    if (! env.isVisible(field)) return failure("field " + clazz.getName() + '.' + name + " is not visible from " + env.thisClass.getName());
+    if (! isStatic(field)) return failure("field " + clazz.getName() + '.' + name + " is not a static field");
+
+    try { return unit(new StaticFieldAccess(field)); } catch (NotFoundException e) {
+      return error(e);
+    }
+  }
 
   private static final PackratParser<NewExpression> newObject =
       bind(prefix("new", className), clazz -> foreach(clazz.getDeclaredConstructors(), c -> effect(map(arguments(c), args -> new NewExpression(c, args)), throwing(c)), "suitable constructor is not found"));
